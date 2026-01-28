77fd2943abe0a481f337bb7479cc4fd33494ac1e
bagoa <bagoa@eszi.hu>
2026-01-28 08:25:03 +0100

    Day 1 of finding my inner cum demons

---
+++ Backend/migrations/20260127090335-create-post.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      contentLong: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      authorName: {
+        type: Sequelize.STRING,
+        defaultValue: "Admin",
+      },
+      imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  }
+};
---
+++ Backend/routes/site.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+require('dotenv').config();
+
+let siteController = require("../controllers/siteController");
+
+router.get("/", siteController.index);
+
+module.exports = router;
