6e1f600f075996e091f4b7cb0c85e44c38a7686a
Bujáki Donát <bujakid@eszi.hu>
2026-02-04 08:46:21 +0100

    mornin

--- mobil/App.js
+++ mobil/App.js
@@ -1,16 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SimpleListScreen from './screens/SimpleListScreen/SimpleListScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SimpleListScreen from "./screens/SimpleListScreen/SimpleListScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import ArticleDetailsScreen from "./screens/ArticleDetailsScreen/ArticleDetailsScreen";
+import ArticleListScreen from "./screens/ArticleListScreen/ArticleListScreen";
+import SearchScreen from "./screens/SearchScreen/SearchScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function ArticleStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ArticleList"
+        component={ArticleListScreen}
+        options={{ title: "Bejegyzesek" }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-  return <SimpleListScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="ArticlesTab"
+          component={ArticleStack}
+          options={{
+            tabBarLabel: "Bejegyzesek",
+            tabBarIcon: ({ color, size }) => {
+              <MaterialIcons name="article" size={size} color={color} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
